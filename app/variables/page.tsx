bd1e675902fbacf8221c8747cec7dac1eecf3a85
superfaz <16510828+superfaz@users.noreply.github.com>
2023-12-21 08:53:13 +0100

    Ignore tokens

--- app/variables/page.tsx
+++ app/variables/page.tsx
@@ -2,12 +2,14 @@ export default function VariablesPage() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-24">
       <h2>Environment variables</h2>
-      {Object.entries(process.env).map(([key, value]) => (
-        <div key={key}>
-          <span className="pe-4">{key}</span>
-          <span>{value}</span>
-        </div>
-      ))}
+      {Object.entries(process.env)
+        .filter(([key, _]) => !key.endsWith("TOKEN"))
+        .map(([key, value]) => (
+          <div key={key}>
+            <span className="pe-4">{key}</span>
+            <span>{value}</span>
+          </div>
+        ))}
     </main>
   );
 }
