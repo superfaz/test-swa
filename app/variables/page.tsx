b7ddc18ad861fb4e4502a9cf16c5ad0c4efebaf4
superfaz <16510828+superfaz@users.noreply.github.com>
2024-01-08 10:39:42 +0100

    Update page.tsx

--- app/variables/page.tsx
+++ app/variables/page.tsx
@@ -3,7 +3,6 @@ export default function VariablesPage() {
     <main className="flex min-h-screen flex-col justify-between p-24">
       <h2>Environment variables</h2>
       {Object.entries(process.env)
-        .filter(([key, _]) => !key.endsWith("TOKEN"))
         .map(([key, value]) => (
           <div key={key}>
             <span className="pe-4">{key}</span>
